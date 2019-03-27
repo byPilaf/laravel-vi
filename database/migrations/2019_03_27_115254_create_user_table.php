a8969e59332be833321fcf0123c0a40a4ee979c2
by <a492942809#live.com>
2019-03-27 18:25:29 +0800

    用户表迁移文件

--- database/migrations/2019_03_27_115254_create_user_table.php
+++ database/migrations/2019_03_27_115254_create_user_table.php
@@ -18,7 +18,7 @@ class CreateUserTable extends Migration
             $table -> string('mobile',11) -> notnull() -> comment('手机号'); 
             $table -> string('password',255) -> notnull() -> comment('用户密码');
             $table -> string('name',20) -> nullable() -> comment('用户昵称');
-            $table -> string('avatar_address',255) -> nullable() -> comment('用户头像路径');
+            $table -> string('avatarUrl',255) -> nullable() -> comment('用户头像路径');
             $table -> string('email',40) -> nullable() -> comment('电子邮箱地址'); 
             $table -> enum('status',['1','2']) -> notnull() -> default('2') -> comment('用户状态,1=停用,2=启用'); 
             $table -> timestamps();
